913fd10e37ef17fe8e5263bf231aa0a0482325d2
oleg-afonick <afonick@mail.ru>
2024-10-03 02:31:42 +0300

    Add filter icontains

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,68 +1,92 @@
-from fastapi import Body, APIRouter
+from fastapi import Body, Query, APIRouter
 
+from src.database import async_session_maker, engine
 from src.api.dependencies import paginator
 from src.schemas.hotels import HotelSchema, HotelSchemaPATCH
+from sqlalchemy import insert, select
+from src.models.hotels import HotelsModel
 
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
 
-hotels = [
-    {"id": 1, "name": "Hotel 1", "address": "Address 1"},
-    {"id": 2, "name": "Hotel 2", "address": "Address 2"},
-    {"id": 3, "name": "Hotel 3", "address": "Address 3"},
-    {"id": 4, "name": "Hotel 4", "address": "Address 4"},
-    {"id": 5, "name": "Hotel 5", "address": "Address 5"},
-    {"id": 6, "name": "Hotel 6", "address": "Address 6"},
-    {"id": 7, "name": "Hotel 7", "address": "Address 7"},
-    {"id": 8, "name": "Hotel 8", "address": "Address 8"},
-    {"id": 9, "name": "Hotel 9", "address": "Address 9"},
-    {"id": 10, "name": "Hotel 10", "address": "Address 10"}
-]
-
 
 @router.get("")
-def get_hotels(pagination: paginator):
-    if pagination.page and pagination.per_page:
-        start = (pagination.page - 1) * pagination.per_page
-        end = start + pagination.per_page
-        paginated_hotels = hotels[start:end]
-        return paginated_hotels
-    return hotels
+async def get_hotels(
+        pagination: paginator,
+        location: str | None = Query(None, description="Адрес отеля"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    per_page = pagination.per_page or 5
+    async with async_session_maker() as session:
+        query = select(HotelsModel)
+        if location:
+            query = query.filter(HotelsModel.location.icontains(location))
+        if title:
+            query = query.filter(HotelsModel.title.icontains(title))
+        query = (
+            query
+            .limit(per_page)
+            .offset((pagination.page - 1) * per_page)
+        )
+        print(query.compile(engine, compile_kwargs={"literal_binds": True}))
+        result = await session.execute(query)
+        hotels = result.scalars().all()
+        return hotels
 
 
 @router.post("")
-def post_hotels(hotel_data: HotelSchema):
-    hotels.append({"id": hotels[-1]["id"] + 1, "name": hotel_data.name, "address": hotel_data.address})
-    return hotels
+async def post_hotels(hotel_data: HotelSchema = Body(openapi_examples={
+    "1": {
+        "summary": "Сочи",
+        "value": {
+            "title": "Отель 5 звезд у Черного моря",
+            "location": "Сочи, ул. Моря, 1"
+        }
+    },
+    "2": {
+        "summary": "Дубай",
+        "value": {
+            "title": "Отель 5 звезд в Персидском заливе",
+            "location": "Дубай, ул. Шейха, 2"
+        }
+    }
+})):
+    async with async_session_maker() as session:
+        insert_hotel_stmt = insert(HotelsModel).values(**hotel_data.model_dump())
+        print(insert_hotel_stmt.compile(engine, compile_kwargs={"literal_binds": True}))
+        await session.execute(insert_hotel_stmt)
+        await session.commit()
+
+    return {"status": "OK"}
 
 
-@router.put("/{hotel_id}")
-def hotels_put(hotel_id: int, hotel_data: HotelSchema):
-    get_hotel = [hotel for hotel in hotels if hotel.get("id") == hotel_id]
-    if get_hotel:
-        hotel = get_hotel[0]
-        hotel["name"] = hotel_data.name
-        hotel["address"] = hotel_data.address
-        return hotels
-    return "Hotel not found"
-
-
-@router.patch("/{hotel_id}")
-def hotels_patch(
-        hotel_id: int, hotel_data: HotelSchemaPATCH):
-    get_hotel = [hotel for hotel in hotels if hotel.get("id") == hotel_id]
-    if get_hotel:
-        hotel = get_hotel[0]
-        hotel["name"] = hotel_data.name if hotel_data.name else hotel["name"]
-        hotel["address"] = hotel_data.address if hotel_data.address else hotel["address"]
-        return hotels
-    return "Hotel not found"
-
-
-@router.delete("/{hotel_id}")
-def hotels_delete(hotel_id: int):
-    get_hotel = [hotel for hotel in hotels if hotel.get("id") == hotel_id]
-    if get_hotel:
-        hotel = get_hotel[0]
-        hotels.remove(hotel)
-        return hotels
-    return "Hotel not found"
+# @router.put("/{hotel_id}")
+# def hotels_put(hotel_id: int, hotel_data: HotelSchema):
+#     get_hotel = [hotel for hotel in hotels if hotel.get("id") == hotel_id]
+#     if get_hotel:
+#         hotel = get_hotel[0]
+#         hotel["name"] = hotel_data.name
+#         hotel["address"] = hotel_data.address
+#         return hotels
+#     return "Hotel not found"
+#
+#
+# @router.patch("/{hotel_id}")
+# def hotels_patch(
+#         hotel_id: int, hotel_data: HotelSchemaPATCH):
+#     get_hotel = [hotel for hotel in hotels if hotel.get("id") == hotel_id]
+#     if get_hotel:
+#         hotel = get_hotel[0]
+#         hotel["name"] = hotel_data.name if hotel_data.name else hotel["name"]
+#         hotel["address"] = hotel_data.address if hotel_data.address else hotel["address"]
+#         return hotels
+#     return "Hotel not found"
+#
+#
+# @router.delete("/{hotel_id}")
+# def hotels_delete(hotel_id: int):
+#     get_hotel = [hotel for hotel in hotels if hotel.get("id") == hotel_id]
+#     if get_hotel:
+#         hotel = get_hotel[0]
+#         hotels.remove(hotel)
+#         return hotels
+#     return "Hotel not found"
